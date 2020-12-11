e1ae003a868d87ab20b25fc6351047cd40080fd1
Felippe Fernandes <felippefsantana02@gmail.com>
2020-12-10 21:26:37 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
